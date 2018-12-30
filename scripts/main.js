14be7f660d5b76a554ed41ae295b68fd8ae19c00
Jin <coutjules@free.fr>
2018-12-30 15:05:38 +0100

    ajout de la boite de dialogue confirm et de la redirection

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,12 @@
 var elem = document.querySelector('html');
 elem.addEventListener('click', function(e) {
-    alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
+  if  (confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please! Tu viens ?')) {
+	  document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+	  } else { 
+	};
+		
 });
 setTimeout(function() {
     elem.dispatchEvent(new Event('click'));
 }, 10000);
+
